01f88306a87c5a14d179247c0e4405b5599ce711
jdmeans6 <jdmeans6@me.com>
2021-10-08 11:24:12 -0500

    start of jensen's coffee app, root & home routes

---
+++ JensenCoffee/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+// 
+const logger = require('morgan');
+app.use(logger('dev'));
+
+// root route
+app.get('/', (req, res) => {
+    res.send('Coffee Time!')
+});
+
+app.get('/home', (req, res) => {
+    res.render('home.ejs')
+});
+
+
+// listener
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
