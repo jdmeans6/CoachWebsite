d4d47bc3322473b7eefcd0878db057fb76f7a669
jdmeans6 <jdmeans6@me.com>
2021-10-11 17:38:52 -0500

    addition of signup page for Coach's Website

--- WeightTrainSite/index.js
+++ WeightTrainSite/index.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const app = express();
 
+// logger
 const logger = require("morgan");
 app.use(logger("dev"));
 
-app.get("/", (req, res) => {
-    res.send("Welcome!")
-});
 
-app.get("/home", (req, res) => {
+// app.get("/", (req, res) => {
+//     res.send("Welcome!")
+// });
+
+app.get("/", (req, res) => {
     res.render("home.ejs")
 });
 
 app.get("/about", (req, res) => {
     res.render("about.ejs")
 });
+
+app.get('/signup', (req, res)=> {
+    res.render('signup.ejs')
+})
 
 app.get("/payment", (req, res) => {
     res.render("payment.ejs")
@@ -24,11 +30,14 @@ app.get("/contact", (req, res) => {
     res.render("contact.ejs")
 });
 
-
+app.get('/routines', (req, res) => {
+    res.render('routines.ejs')
+});
 
 app.get("*", (req, res) => {
     res.send("I'm sorry, this page does not exist.")
 });
 
+// listener
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App is up and running on ${PORT}`));
