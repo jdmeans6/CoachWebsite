e742b58e3bc7b7858110aa48f10dbaafd5f386a8
jdmeans6 <jdmeans6@me.com>
2021-10-18 21:20:34 -0500

    addition of js concat/reverse/push methods practice

---
+++ CoderVox/4_Javascript/04_jsArrays/concatMethod.js
@@ -0,0 +1,7 @@
+// 11.3 Concat Method
+// this method combines 2 or more arrays
+var bakery1 = ['muffin', 'croissant', 'cookie']
+var bakery2 = ['bagel', 'brownie']
+
+var newBakery = bakery1.concat(bakery2);
+console.log(newBakery);     // this logs an array of all 5 items with bakery2 items adding to the end of the list
---
+++ CoderVox/4_Javascript/04_jsArrays/reverseMethod.js
@@ -0,0 +1,6 @@
+// 11.3 Reverse Method
+// this reverses the elements in the array, and DOES alter the original array
+var food = ['pizza', 'burger', 'taco', 'pizza', 'spaghetti']
+
+var foodReverse = food.reverse()
+console.log(foodReverse)        // this will log the array in reverse order
