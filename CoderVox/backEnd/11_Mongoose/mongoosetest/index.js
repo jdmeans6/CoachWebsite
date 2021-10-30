4772e7f43d12f9772985dcc63ebce106f2702e87
jdmeans6 <jdmeans6@me.com>
2021-10-30 13:27:58 -0500

    keeping tree up to date

--- CoderVox/backEnd/11_Mongoose/mongoosetest/index.js
+++ CoderVox/backEnd/11_Mongoose/mongoosetest/index.js
@@ -14,6 +14,21 @@ const pizzaSchema = new mongoose.Schema({
 });
 // next you create a model, which is used to make a document
     // first paramater is the singular name of your collection
-        // in this case "Pizza" will become the pizza collection inside the database
+        // mongoose will create a plural, lowecase version of it: in this case "Pizza" will become the "pizzas" collection inside the database
     // the second paramater is the schema
 const Pizza = mongoose.model('Pizza', pizzaSchema);
+
+// next you create a document by creating a new model from above and passing an object into it that follows the schema from above
+const thePizza = new Pizza ({
+    name: 'Pepperoni',
+    price: 10
+});
+// then, to save this document the following runs
+    // it can be run without the callback function, but it is nice to have to see if its been done successfully
+thePizza.save(function(error, result) {
+    if(error){
+        console.log(error)  // function will log an error in console if it does not save correctly
+    } else {
+        console.log(result) // function logs the result if it is successful
+    }
+})
